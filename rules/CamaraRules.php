7acc3145f01564560c46ff8430309b47fcdabdf4
Everton da Rosa <everton3x@gmail.com>
2021-05-04 09:43:09 -0300

    Adicionada regra para testar se existe receita arrecadada na câmara

--- rules/CamaraRules.php
+++ rules/CamaraRules.php
@@ -9,9 +9,22 @@ namespace IDDRS\ContTest\Rules;
  */
 trait CamaraRules {
 
-    public function testExistenciaDeReceitaOrcamentariaNaCamaraTestadaManualmente() {
+    /*public function testExistenciaDeReceitaOrcamentariaNaCamaraTestadaManualmente() {
         $this->assertTrue(true);
         $this->conferenciaExterna(__METHOD__, '6.2.1');
+    }*/
+    
+    public function testReceitaArrecadadaIgualAZero() {
+        $filter = function (array $line): bool {
+            if (str_starts_with($line['conta_contabil'], '6.2.1.2') && $line['escrituracao'] === 'S') {
+                return true;
+            }
+            return false;
+        };
+        $saldoDevedor = $this->somaColuna($this->getDataFrame('BAL_VER'), 'saldo_atual_debito', $filter);
+        $saldoCredor = $this->somaColuna($this->getDataFrame('BAL_VER'), 'saldo_atual_credito', $filter);
+        
+        $this->comparar(0.0, ($saldoCredor - $saldoDevedor));
     }
 
 }
